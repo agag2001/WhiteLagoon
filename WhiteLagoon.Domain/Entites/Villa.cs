3ebca353c2315e316b2f5af5ffffaa44bf1b16ca
abdallah agag <bodyagag@gmail.com>
2024-12-06 04:53:06 +0200

    Villa Model Validation

--- WhiteLagoon.Domain/Entites/Villa.cs
+++ WhiteLagoon.Domain/Entites/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,18 @@ namespace WhiteLagoon.Domain.Entites
 {
     public class Villa
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [MaxLength(40)]
         public required string Name {  get; set; }      
-        public string? Description {  get; set; }   
+        public string? Description {  get; set; }
+
+        [Display(Name = "Price Per Night")]
+        [Range(10,100000)]
         public double Price { get; set; }   
-        public int Sqft {  get; set; }      
-        public int Occupancy {  get; set; }    
+        public int Sqft {  get; set; }
+        [Range(1,10)]
+        public int Occupancy {  get; set; }
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }  
         public DateTime? CreatedDate {  get; set; } 
         public DateTime? UpdatedDate { get;set; }
---
+++ WhiteLagoon/Controllers/VillaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WhiteLagoon.Domain.Entites;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Controllers
+{
+  
+    public class VillaController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VillaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public AppDbContext Context { get; }
+
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();   
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name== obj.Description)
+            {
+                ModelState.AddModelError("", "Name can't be the same as Description");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View(obj);
+          
+        }
+    }
+}
